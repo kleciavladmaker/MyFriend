996af77fb6e2f1369beda9bea9cc075ae68f5dfe
Operador <Operador@DESKTOP-FP7N6BR>
2020-04-08 21:41:33 -0300

    feat: Cria variaveis

--- src/myfriend/CadastroUsuario.java
+++ src/myfriend/CadastroUsuario.java
@@ -5,10 +5,85 @@
  */
 package myfriend;
 
+import java.util.Date;
+
 /**
  *
  * @author Operador
  */
-public class CadastroUsuario {
+    /**
+     *
+     */
+    public class CadastroUsuario {
+    private String nome;
+    private final Date nascimento;
+    private final String endereco;
+    private String cnpj;
+    private String cpf;
+    private String email;
+    private final String telefone;     
+  
+
+
+public CadastroUsuario(String nome, Date nascimento,String endereco, String cnpj, String cpf, String email, String telefone){
     
+   this.nome = nome;   //this refere-se a si proprio, ele é necessário em situações que vc precisa referenciar o proprio objeto//
+   this.nascimento = nascimento; // o this você identifica a variavel local da classe//
+   this.endereco = endereco;
+   this.cnpj = cnpj;
+   this.cpf = cpf;
+   this.email = email;
+   this.telefone =  telefone;
+   
 }
+
+ public String getNome() {
+        return nome;
+    }
+ 
+ public Date getNascimento() {
+        return nascimento;
+    }
+ 
+  public String getEndereco() {
+        return endereco;
+    }
+  
+  public String getCnpj() {
+        return cnpj;
+    }
+  
+  public String getCpf() {
+        return cpf;
+    }
+  
+   public String getEmail() {
+        return email;
+    }
+   
+   public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  
+    }
+
+
+
+
+
+
+    
