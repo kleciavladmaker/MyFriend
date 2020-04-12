0b62cdcf4fab9012d4bde3149e876df51fca2203
gusta <gusta@DESKTOP-R66MK8E>
2020-04-11 22:51:05 -0300

    Fazer Class Login e SqlQuery e seu metos e variáveis obs: deixar o codigo comentado

---
+++ src/myfriend/Login.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfriend;
+
+/**
+ *
+ * @author Gustavo Guimarães
+ */
+import myfriend.SqlQuery;
+import myfriend.ContaUsuario;
+
+public class Login {
+    //Vatiáveis ----------------------------------------------------------------------------------------------
+    private String login;
+    private String senha;
+    //--------------------------------------------------------------------------------------------------------
+    
+    //Metodos ------------------------------------------------------------------------------------------------
+    public void Login(String login, String senha)
+    {
+        this.login = login;
+        this.senha = senha;
+    }
+    
+    public String getLogin()
+    {
+        return login;
+    }
+    
+    public String getSenha()
+    {
+        return senha;
+    }
+    
+    public void setLogin(String x)
+    {
+        login = x;
+    }
+    
+    public void setSenha(String x)
+    {
+        senha = x;
+    }
+    
+    public void logarUsuario()
+    {
+        SqlQuery sq = new SqlQuery();
+        sq.executarVerificacao(getLogin(), getSenha());
+    }
+    //---------------------------------------------------------------------------------------------------------
+}
