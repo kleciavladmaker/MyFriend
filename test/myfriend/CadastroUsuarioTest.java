9268847a8cde613afff13d7c915f01385a659b60
Júlia Brandão <Operador@192.168.0.5>
2020-05-29 17:40:26 -0300

    feat: Criação de ponteiro nulo

---
+++ test/myfriend/CadastroUsuarioTest.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfriend;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Operador
+ */
+public class CadastroUsuarioTest {
+    
+    public CadastroUsuarioTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNome method, of class CadastroUsuario.
+     */
+    @Test
+    public void testGetNome() {
+        System.out.println("getNome");
+        CadastroUsuario instance = null;
+        String expResult = "";
+        @SuppressWarnings("null")
+        String result = instance.getNome();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNascimento method, of class CadastroUsuario.
+     */
+    @Test
+    public void testGetNascimento() {
+        System.out.println("getNascimento");
+        CadastroUsuario instance = null;
+        Date expResult = null;
+        @SuppressWarnings("null")
+        Date result = instance.getNascimento();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEndereco method, of class CadastroUsuario.
+     */
+    @Test
+    public void testGetEndereco() {
+        System.out.println("getEndereco");
+        CadastroUsuario instance = null;
+        String expResult = "";
+        @SuppressWarnings("null")
+        String result = instance.getEndereco();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCnpj method, of class CadastroUsuario.
+     */
+    @Test
+    public void testGetCnpj() {
+        System.out.println("getCnpj");
+        CadastroUsuario instance = null;
+        String expResult = "";
+        @SuppressWarnings("null")
+        String result = instance.getCnpj();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCpf method, of class CadastroUsuario.
+     */
+    @Test
+    public void testGetCpf() {
+        System.out.println("getCpf");
+        CadastroUsuario instance = null;
+        String expResult = "";
+        @SuppressWarnings("null")
+        String result = instance.getCpf();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEmail method, of class CadastroUsuario.
+     */
+    @Test
+    public void testGetEmail() {
+        System.out.println("getEmail");
+        CadastroUsuario instance = null;
+        String expResult = "";
+        @SuppressWarnings("null")
+        String result = instance.getEmail();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTelefone method, of class CadastroUsuario.
+     */
+    @Test
+    public void testGetTelefone() {
+        System.out.println("getTelefone");
+        CadastroUsuario instance = null;
+        String expResult = "";
+        @SuppressWarnings("null")
+        String result = instance.getTelefone();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNome method, of class CadastroUsuario.
+     */
+    @Test
+    @SuppressWarnings("null")
+    public void testSetNome() {
+        System.out.println("setNome");
+        String nome = "";
+        CadastroUsuario instance = null;
+        instance.setNome(nome);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmail method, of class CadastroUsuario.
+     */
+    @Test
+    @SuppressWarnings("null")
+    public void testSetEmail() {
+        System.out.println("setEmail");
+        String email = "";
+        CadastroUsuario instance = null;
+        instance.setEmail(email);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
